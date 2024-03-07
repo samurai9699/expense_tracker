5b12c28af0a0878d9e74062e917b5ff6e1bd10fb
sam muriithi <samuelmuriithi965@gmail.com>
2024-03-07 16:11:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+![Demo Site](https://i.ibb.co/ygxgbNm/Screenshot-11.png)
+
+
+This is a MERN GraphQL Expense Tracker Application. This project is intended to assist in the efficient management of expenses through the utilization of a modern technology stack and GraphQL.
+
+## Features:
+
+-- 🌟 Technology stack: MERN (MongoDB, Express.js, React.js, Node.js) with Apollo GraphQL
+-- 📝 Understanding type definitions and resolvers to define the GraphQL schema and implement data fetching logic
+-- 🔄 Implementing mutations to modify data in the GraphQL API and establish graph relationships
+-- 🎃 Implementing authentication with Passport.js and utilizing MongoDB as the session store
+-- 🚀 Utilizing Apollo Client for global state management
+-- 🐞 Implementing error handling on both the server and client sides
+-- ⭐ Streamlining deployment with a platform called Render
+-- 👾 Setting up cron jobs for scheduled tasks and automation
+
+
+The setup requires creating a .env file with the following content:
+
+MONGO_URI=...
+SESSION_SECRET=...
+
+
+After setting up the .env file, you can build the app using the following command:
+
+npm run build
+
+
+Finally, start the app by running:
+
+npm start
---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+
+dotenv.config();
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+})
+
+// Ensure we wait for our server to start
+await server.start();
+
+// and our expressMiddleware function.
+app.use(
+    '/',
+    cors(),
+    express.json(),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ req }),
+    }),
+  );
+  
+  // Modified server startup
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  
+  console.log(`🚀 Server ready at http://localhost:4000/`);
